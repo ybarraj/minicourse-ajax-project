06e0474bc81f11d0d4039004abebf206643d3787
ybarraj <josephybarra88@gmail.com>
2016-10-28 11:22:35 -0600

    should be able to find new addresses but doesn't work

--- js/script.js
+++ js/script.js
@@ -11,6 +11,15 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+    var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
     // load streetview
 
     // YOUR CODE GOES HERE!
